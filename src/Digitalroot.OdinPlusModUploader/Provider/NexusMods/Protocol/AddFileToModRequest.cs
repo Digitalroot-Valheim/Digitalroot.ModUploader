fd375dd5a69c794d99cb9bd3f46dc04276c78ce9
Nicholas Dunnaway <nick@dunnaway.org>
2021-12-09 20:24:37 -0800

    Update AddFileToModRequest

--- src/Digitalroot.OdinPlusModUploader/Provider/NexusMods/Protocol/AddFileToModRequest.cs
+++ src/Digitalroot.OdinPlusModUploader/Provider/NexusMods/Protocol/AddFileToModRequest.cs
@@ -12,10 +12,10 @@ internal class AddFileToModRequest : AbstractAuthorizedRequest
     AddParameter("game_id", addFileToModRequestModel.GameId);
     AddParameter("name", addFileToModRequestModel.Name);
     AddParameter("file-version", addFileToModRequestModel.FileVersion);
-    AddParameter("update-version", addFileToModRequestModel.UpdateVersion);
+    AddParameter("update-version", Convert.ToInt32(addFileToModRequestModel.UpdateVersion));
     AddParameter("category", Convert.ToInt32(addFileToModRequestModel.Category));
     AddParameter("brief-overview", addFileToModRequestModel.BriefOverview); // None? Can be empty
-    AddParameter("set_as_main_nmm", addFileToModRequestModel.SetAsMainNmm);
+    AddParameter("set_as_main_nmm", Convert.ToInt32(addFileToModRequestModel.SetAsMainNmm));
     AddParameter("file_uuid", addFileToModRequestModel.FileUUID);
     AddParameter("file_size", addFileToModRequestModel.FileSize.ToString());
     AddParameter("mod_id", addFileToModRequestModel.ModID.ToString());
@@ -23,13 +23,12 @@ internal class AddFileToModRequest : AbstractAuthorizedRequest
     AddParameter("action", addFileToModRequestModel.Action);
     AddParameter("uploaded_file", addFileToModRequestModel.UploadedFile);
     AddParameter("original_file", addFileToModRequestModel.OriginalFile);
-    AddParameter("requirements_pop_up", addFileToModRequestModel.RequirementPopUp);
-    
-    AddParameter("remove_nmm_button", addFileToModRequestModel.RemoveNmmButton);
+    AddParameter("requirements_pop_up", Convert.ToInt32(addFileToModRequestModel.RequirementPopUp));
+    AddParameter("remove_nmm_button", Convert.ToInt32(addFileToModRequestModel.RemoveNmmButton));
     if (addFileToModRequestModel.UpdateVersion)
     {
-      AddParameter("new-existing", addFileToModRequestModel.NewExisting);
-      AddParameter("remove-old-version", addFileToModRequestModel.RemoveOldVersion);
+      AddParameter("new-existing", Convert.ToInt32(addFileToModRequestModel.NewExisting));
+      AddParameter("remove-old-version", Convert.ToInt32(addFileToModRequestModel.RemoveOldVersion));
       if (addFileToModRequestModel.OldFileId != null) AddParameter("old_file_id", addFileToModRequestModel.OldFileId); // 7515
     }
     
