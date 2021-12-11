71e74348b4bd41a0f6889b0066b2f767c92a0481
Nicholas Dunnaway <nick@dunnaway.org>
2021-12-11 10:39:35 -0800

    Fixed wording

--- src/Digitalroot.OdinPlusModUploader/Utils/Credits.cs
+++ src/Digitalroot.OdinPlusModUploader/Utils/Credits.cs
@@ -10,14 +10,14 @@ namespace Digitalroot.OdinPlusModUploader.Utils
     internal static void PrintCredits()
     {
       Console.WriteLine();
-      Console.WriteLine($"Original author {"Alistair Chapman".Pastel(ColorOptions.WarningColor)} aka {"agc93".Pastel(ColorOptions.WarningColor)} ({"https://github.com/agc93".Pastel(ColorOptions.SuccessColor)}).".Pastel(ColorOptions.InfoColor));
+      Console.WriteLine($"Based on work by the original author {"Alistair Chapman".Pastel(ColorOptions.WarningColor)} aka {"agc93".Pastel(ColorOptions.WarningColor)} ({"https://github.com/agc93".Pastel(ColorOptions.SuccessColor)}).".Pastel(ColorOptions.InfoColor));
       Console.WriteLine($"View original @ {"https://github.com/agc93/nexus-uploader".Pastel(ColorOptions.SuccessColor)}, MIT License.".Pastel(ColorOptions.InfoColor));
       Console.WriteLine($"Contributors:".Pastel(ColorOptions.InfoColor));
       Console.WriteLine($"\t{"focustense".Pastel(ColorOptions.WarningColor)} ({"https://github.com/focustense".Pastel(ColorOptions.SuccessColor)}).".Pastel(ColorOptions.InfoColor));
       Console.WriteLine($"\t{"Timothy Baldridge".Pastel(ColorOptions.WarningColor)} aka {"halgari".Pastel(ColorOptions.WarningColor)} ({"https://github.com/halgari".Pastel(ColorOptions.SuccessColor)}).".Pastel(ColorOptions.InfoColor));
       Console.WriteLine($"\t{"Digitalroot".Pastel(ColorOptions.WarningColor)} ({"https://github.com/Digitalroot".Pastel(ColorOptions.SuccessColor)}).".Pastel(ColorOptions.InfoColor));
       Console.WriteLine();
-      Console.WriteLine($"OdinPlusModUploader created by {"Digitalroot".Pastel(ColorOptions.WarningColor)} of the {"Odin Plus Team.".Pastel(ColorOptions.WarningColor)}".Pastel(ColorOptions.InfoColor));
+      Console.WriteLine($"OdinPlus Mod Uploader created by {"Digitalroot".Pastel(ColorOptions.WarningColor)} of the {"OdinPlus Team.".Pastel(ColorOptions.WarningColor)}".Pastel(ColorOptions.InfoColor));
       Console.WriteLine($"Released under the {"MIT License".Pastel(ColorOptions.WarningColor)}.".Pastel(ColorOptions.InfoColor));
       Console.WriteLine($"Support Me @ {"https://www.buymeacoffee.com/digitalroot".Pastel(ColorOptions.SuccessColor)}.".Pastel(ColorOptions.InfoColor));
       Console.WriteLine($"Join us on Discord: {"https://discord.gg/mbkPcvu9ax".Pastel(ColorOptions.SuccessColor)}.".Pastel(ColorOptions.InfoColor));
@@ -26,7 +26,7 @@ namespace Digitalroot.OdinPlusModUploader.Utils
     internal static void PrintWelcome()
     {
       var appVersion = Assembly.GetExecutingAssembly().GetName().Version;
-      Console.WriteLine($"Odin Plus Mod Uploader ({appVersion?.Major}.{appVersion?.Minor}.{appVersion?.Build})".Pastel(ColorOptions.InfoColor));
+      Console.WriteLine($"OdinPlus Mod Uploader ({appVersion?.Major}.{appVersion?.Minor}.{appVersion?.Build})".Pastel(ColorOptions.InfoColor));
       Console.WriteLine($"By: {"Digitalroot".Pastel(ColorOptions.WarningColor)}".Pastel(ColorOptions.InfoColor));
       Console.WriteLine();
     }
